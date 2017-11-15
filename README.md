553b3ec6a2411b634c357ee30158439279052497
Taron Foxworth <taronfoxworth@Tarons-MacBook-Pro.local>
2017-11-15 15:19:30 -0500

    Clean up and document things

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# example-modbus
+# Losant Example Using Modbus
+
+This project is an example of a Modbus application reading from a PLC and sending data to Losant via MQTT. 
+
+More details coming soon.
--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-var modbus = require('jsmodbus');
+var Modbus = require('jsmodbus');
 var Device = require('losant-mqtt').Device;
 
+// IP Address of PLC
 var host = 'PLC_IP_ADDRESS';
+// Modbus TCP Port (Normally 502)
 var port = 502;
 
-// Construct device.
+// Construct a Losant device.
+// LOSANT_DEVICE_ID, LOSANT_ACCESS_KEY, & LOSANT_ACCESS_SECRET are obtained from Losant
 var device = new Device({
   id: 'LOSANT_DEVICE_ID',
   key: 'LOSANT_ACCESS_KEY',
   secret: 'LOSANT_ACCESS_SECRET'
 });
 
-var client = modbus.client.tcp.complete({
+var client = Modbus.client.tcp.complete({
   'host': host,
   'port': port,
   'autoReconnect': true,
@@ -20,7 +23,9 @@ var client = modbus.client.tcp.complete({
   'unitId': 0
 });
 
+// Connect to PLC.
 client.connect();
+
 // Connect to Losant.
 device.connect(function (error) {
   if (error) {
@@ -28,51 +33,38 @@ device.connect(function (error) {
     throw error;
   }
   // Successfully connected
-  console.log('Connected to Losant')
+  console.log('Connected to Losant');
 });
 
+// Handle PLC Connection
 client.on('connect', function () {
-  console.log('Connected to PLC')
-  setInterval(() => {
-    client.readCoils(0, 15).then(function (resp) {
-      var state = resp.coils.reduce((object, coil, index) => {
-        object[`output-${index}`] = coil;
-        return object
-      }, {})
-      device.sendState(state);;
-    }, console.error);
-  }, 1000);
+  console.log('Connected to PLC');
 
+  // Read Discrete Inputs 0-15 every second 
   setInterval(() => {
     client.readDiscreteInputs(0, 15).then(function (resp) {
       var state = resp.coils.reduce((object, coil, index) => {
         object[`input-${index}`] = coil;
         return object
       }, {})
-      device.sendState(state);;
+      // Report all Discrete Inputs to Losant.
+      device.sendState(state);
     }, console.error);
   }, 1000);
 
-  // Listen for commands.
+  // Listen for Losant commands.
   device.on('command', function (command) {
-    console.log('Command received.');
-    console.log(command.name);
-    console.log(command.payload);
-
+    // The command 'output' will write to a single coil.
     if (command.name = 'output') {
+      // command.payload.coil and command.payload.value are apart of the device command from Losant.
       client.writeSingleCoil(command.payload.coil, command.payload.value).then(function (resp) {
-        // resp will look like { fc: 5, byteCount: 4, outputAddress: 5, outputValue: true }
-        console.log(resp);
+        console.log(`Wrote the vale "${command.payload.value}" to coil "${command.payload.coil}" `);
       }, console.error);
     }
   });
-
-
-
 });
 
+// Log errors
 client.on('error', function (err) {
-
   console.log(err);
-
 })
